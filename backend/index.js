a6a61e3b890a4e645496cbb2321770f3a5190202
Maksim Chernev <m.chernev@mail.ru>
2023-11-12 17:23:22 +0300

    auth n mail

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,25 @@
+require("dotenv").config();
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
-const authRouter = require("./authRouter");
-const PORT = process.env.BACK_PORT || 6000;
+const router = require("./router/index");
+const errorMiddleware = require("./middlewares/error-middleware");
+
+const PORT = process.env.BACK_PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use("/auth", authRouter);
+app.use(cookieParser());
+app.use("/api", router);
+app.use(errorMiddleware);
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
-    app.listen(PORT, () => console.log(`Started on port ${PORT}`));
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
   } catch (e) {
     console.log(e);
   }
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,10 @@
 const { Schema, model } = require("mongoose");
 
-const User = new Schema({
-  username: { type: String, unique: true, required: true },
+const UserSchema = new Schema({
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
-  roles: [{ type: String, ref: "Role" }],
+  isActivated: { type: Boolean, default: false },
+  activationLink: { type: String },
 });
 
-module.exports = model("User", User);
+module.exports = model("User", UserSchema);
